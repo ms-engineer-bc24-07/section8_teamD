f9d8f20179ca97be778714438e41bf88a5fcf5e1
m-maekakuchi <m.marikoromo@gmail.com>
2024-10-31 19:19:55 +0900

    update.DBのパラメータ追加

--- firebase/main.py
+++ firebase/main.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from google.cloud import firestore
 
 def upload_to_firestore(user_id, recipe_id, recipe_title, recipe_url, food_image_url):
@@ -6,6 +7,14 @@ def upload_to_firestore(user_id, recipe_id, recipe_title, recipe_url, food_image
     # JSONファイルを用いて Firestore に接続
     db = firestore.Client.from_service_account_json(key_gcf)
     # 保存したいデータを辞書型で用意
-    data = {"userID":user_id, "recipeId": recipe_id, "recipeTitle": recipe_title, "recipeURL":recipe_url, "foodImageURL":food_image_url}
+    data = {
+        "userID":user_id,
+        "recipeId": recipe_id,
+        "recipeTitle": recipe_title,
+        "recipeURL":recipe_url,
+        "foodImageURL":food_image_url,
+        "created_at": datetime.now(),
+        "updated_at": datetime.now(),
+    }
     # データを保存
     db.collection("recipes").add(data)
